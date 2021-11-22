70ee169ebbe17944b9242f37e86259a2d9e6dff4
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-11-22 15:05:51 -0600

    update

--- data_prep/word_matrix.py
+++ data_prep/word_matrix.py
@@ -4,7 +4,7 @@ import os
 from global_settings import DATA_PATH
 from global_settings import RICH_PATH
 from global_settings import WORD_PATH
-
+import datetime
 
 # https://github.com/MengLingchao/Chinese_financial_sentiment_dictionary
 xlsx_dict = pd.ExcelFile(os.path.join(DATA_PATH, "Chinese_Dict.xlsx"))
@@ -13,17 +13,17 @@ neg_dict = [_.strip() for _ in xlsx_dict.parse("negative").iloc[:, 0]]
 full_dict = pos_dict + neg_dict
 
 
-def compute_matrix(sub_file_rich):
+def build_word(sub_file_rich):
     """ compute word matrix
     :param sub_file_rich: enriched sub file
     """
 
     # load sub file enriched
     sub_df_rich = pd.read_csv(os.path.join(RICH_PATH, sub_file_rich))
-    def func(df): df["text"] if df["title"] is np.nan else " ".join([df["title"], df["text"]])
-    sub_df_rich["article"] = sub_df_rich.apply(func, axis=1)
 
-    sub_word_matrix = sub_df_rich.loc[:, "article"].apply(lambda _: [_.count(word) for word in full_dict])
+    print(f"{datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')} Working on {sub_file_rich}")
+    def func(df): return df["text"] if df["title"] is np.nan else " ".join([df["title"], df["text"]])
+    sub_word_matrix = sub_df_rich.apply(func, axis=1).apply(lambda _: [_.count(word) for word in full_dict])
     sub_word_df = pd.DataFrame(sub_word_matrix.tolist(), columns=full_dict)
 
     sub_word_file = f"word_{sub_file_rich.split('.')[0].split('_')[1]}.csv"
--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from data_prep.data_clean import save_data
 from data_prep.data_split import split_data
 from data_prep.data_clean import clean_data
 from data_prep.data_enrich import enrich_data
+from data_prep.word_matrix import build_word
 from multiprocessing.pool import Pool
 
 
@@ -25,7 +26,6 @@ if not os.path.isdir(WORD_PATH):
     os.mkdir(WORD_PATH)
 
 
-# Data cleaning
 if __name__ == "__main__":
     # clean data
     # raw_file = "raw.csv"
@@ -39,13 +39,25 @@ if __name__ == "__main__":
     # split_data(clean_file, split_num=split_num)
 
     # enrich data
-    sub_file_li = [_.split("/")[-1] for _ in glob.glob(os.path.join(CLEAN_PATH, "*"))]
-    rich_idx_li = [_.split("/")[-1].split(".")[0].split("_")[1] for _ in glob.glob(os.path.join(RICH_PATH, "*"))]
-    sub_file_li = sorted([_ for _ in sub_file_li if _.split(".")[0].split("_")[1] not in rich_idx_li])
+    # sub_file_clean_li = [_.split("/")[-1] for _ in glob.glob(os.path.join(CLEAN_PATH, "*"))]
+    # sub_file_rich_idx = [_.split("/")[-1].split(".")[0].split("_")[1] for _ in glob.glob(os.path.join(RICH_PATH, "*"))]
+    # sub_file_clean_li = sorted([_ for _ in sub_file_clean_li if _.split(".")[0].split("_")[1] not in sub_file_rich_idx])
+    # num_proc = 8
+    #
+    # for idx in range(0, len(sub_file_clean_li), num_proc):
+    #     pool = Pool(num_proc)
+    #     pool.map(enrich_data, sub_file_clean_li[idx: idx + num_proc])
+    #     pool.close()
+    #     pool.join()
+
+    # build word matrix
+    sub_file_rich_li = [_.split("/")[-1] for _ in glob.glob(os.path.join(RICH_PATH, "*"))]
+    sub_word_file_idx = [_.split("/")[-1].split(".")[0].split("_")[1] for _ in glob.glob(os.path.join(WORD_PATH, "*"))]
+    sub_file_rich_li = sorted([_ for _ in sub_file_rich_li if _.split(".")[0].split("_")[1] not in sub_word_file_idx])
     num_proc = 8
 
-    for idx in range(0, len(sub_file_li), num_proc):
+    for idx in range(0, len(sub_file_rich_li), num_proc):
         pool = Pool(num_proc)
-        pool.map(enrich_data, sub_file_li[idx: idx + num_proc])
+        pool.map(build_word, sub_file_rich_li[idx: idx + num_proc])
         pool.close()
         pool.join()
