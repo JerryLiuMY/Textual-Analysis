acaf66ab52998abdfa1883405d8727f9b5b97a32
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-12-20 17:13:56 -0600

    Create sentiment.py
    Create file for sentiment analysis

---
+++ summary/sentiment.py
@@ -0,0 +1,30 @@
+import numpy as np
+from global_settings import full_dict
+
+
+def ssestm_sentiment(O_hat):
+    """ parameters
+    :param: estimated O_hat
+    """
+
+    # "涨" and "跌" with large positive/negative sentiments
+    # "涨" and "跌" with high occurrence
+    sentiment = 0.5 * (O_hat[:, 0] - O_hat[:, 1])
+    occurrence = 0.5 * (O_hat[:, 0] + O_hat[:, 1])
+    idx = np.argsort(sentiment)
+
+    rank_dict = np.array(full_dict)[idx]
+    sentiment = sentiment[idx]
+    occurrence = occurrence[idx]
+
+    return rank_dict, sentiment, occurrence
+
+
+# sharpe
+# long/short turnover
+# find sentiment charged words
+# 沪深300
+# 停牌 delisting
+
+# BERT and doc2vec parts done
+# news assimilation
