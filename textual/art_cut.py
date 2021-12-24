229e6a79473340ed1bf96d37f383db443ff324b2
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2021-12-24 00:12:47 -0600

    update textual
    add art_cut and modify word_sps

---
+++ textual/art_cut.py
@@ -0,0 +1,50 @@
+from global_settings import RICH_PATH, DATA_PATH
+from global_settings import stop_list
+from global_settings import full_dict
+from tools.text_tools import join_tt
+from datetime import datetime
+import pandas as pd
+import pickle
+import jieba
+import math
+import os
+import re
+
+
+def build_art_cut(sub_file_rich):
+    """ compute textual for doc2vec
+    :param sub_file_rich: enriched sub file
+    :return: estimated O_hat
+    """
+
+    # load sub_df_rich
+    textual_name = "art_cut"
+    text_path = os.path.join(DATA_PATH, textual_name)
+    sub_df_rich = pd.read_csv(os.path.join(RICH_PATH, sub_file_rich))
+    sub_df_rich["title"] = sub_df_rich["title"].astype(str)
+    sub_df_rich["text"] = sub_df_rich["text"].astype(str)
+
+    # configure jieba
+    for word in full_dict:
+        jieba.add_word(word)
+
+    def cut_art(art): return [_ for _ in " ".join(jieba.cut(art, cut_all=False, HMM=True)).split()]
+
+    # cut article
+    mini_size = 100
+    sub_art_cut = pd.Series(dtype=object)
+
+    for idx, iloc in enumerate(range(0, sub_df_rich.shape[0], mini_size)):
+        print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
+              f"Working on {sub_file_rich} -- progress {idx + 1} / {math.ceil(sub_df_rich.shape[0] / mini_size)}")
+
+        mini_df_rich = sub_df_rich.iloc[iloc: iloc + mini_size, :].reset_index(inplace=False, drop=True)
+        mini_art_cut = mini_df_rich.apply(join_tt, axis=1).apply(cut_art)
+        mini_art_cut = mini_art_cut.apply(lambda _: [w for w in _ if len(re.findall(r'[\u4e00-\u9fff]+', w)) != 0])
+        mini_art_cut = mini_art_cut.apply(lambda _: [w for w in _ if w not in stop_list])
+        sub_art_cut = pd.concat([sub_art_cut, mini_art_cut], axis=0)
+
+    sub_text_file = f"{textual_name}_{sub_file_rich.split('.')[0].split('_')[1]}.pkl"
+    print(f"Saving to {sub_text_file}...")
+    with open(os.path.join(text_path, sub_text_file), "wb") as f:
+        pickle.dump(sub_art_cut, f)
--- textual/word_sps.py
+++ textual/word_sps.py
@@ -1,7 +1,9 @@
 from scipy.sparse import csr_matrix, save_npz
 from global_settings import full_dict, RICH_PATH, DATA_PATH
-import pandas as pd
+from tools.text_tools import join_tt
 from datetime import datetime
+import pandas as pd
+import numpy as np
 import math
 import os
 
@@ -11,17 +13,16 @@ def build_word_sps(sub_file_rich):
     :param sub_file_rich: enriched sub file
     """
 
-    # load sub file enriched
+    # load sub_df_rich
     textual_name = "word_sps"
     text_path = os.path.join(DATA_PATH, textual_name)
     sub_df_rich = pd.read_csv(os.path.join(RICH_PATH, sub_file_rich))
     sub_df_rich["title"] = sub_df_rich["title"].astype(str)
     sub_df_rich["text"] = sub_df_rich["text"].astype(str)
-    def join_tt(df): return df["text"] if df["title"] == "nan" else " ".join([df["title"], df["text"]])
 
     # build word matrix
     mini_size = 100
-    sub_word_df = pd.DataFrame()
+    sub_word_mtx = np.empty([0, len(full_dict)])
 
     for idx, iloc in enumerate(range(0, sub_df_rich.shape[0], mini_size)):
         print(f"{datetime.now().strftime('%Y-%m-%d %H:%M:%S')} "
@@ -29,10 +30,9 @@ def build_word_sps(sub_file_rich):
 
         mini_df_rich = sub_df_rich.iloc[iloc: iloc + mini_size, :].reset_index(inplace=False, drop=True)
         mini_word_mtx = mini_df_rich.apply(join_tt, axis=1).apply(lambda _: [_.count(word) for word in full_dict])
-        mini_word_df = pd.DataFrame(mini_word_mtx.tolist(), columns=full_dict)
-        sub_word_df = sub_word_df.append(mini_word_df)
+        sub_word_mtx = np.vstack([sub_word_mtx, np.array(mini_word_mtx.tolist())])
 
-    sub_word_sps = csr_matrix(sub_word_df.values)
+    sub_word_sps = csr_matrix(sub_word_mtx)
     sub_text_file = f"{textual_name}_{sub_file_rich.split('.')[0].split('_')[1]}.npz"
     print(f"Saving to {sub_text_file}...")
     save_npz(os.path.join(text_path, sub_text_file), sub_word_sps)
