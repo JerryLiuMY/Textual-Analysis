83872325ab34f841ecba2848ea7eed97e5dd7127
JerryLiuMY <42907682+JerryLiuMY@users.noreply.github.com>
2022-05-08 20:19:16 -0500

    Update params.py

--- params/params.py
+++ params/params.py
@@ -9,13 +9,13 @@ window_dict = {
 
 proc_dict = {
     "ssestm": 37,
-    "doc2vec": 13,
+    "doc2vec": 19,
     "bert": 1,
 }
 
 params_dict = {
     "ssestm": {"pen": [0.0]},
-    "doc2vec": {"window": [5, 10, 20], "vec_size": [5, 10, 20], "epochs": [20],
+    "doc2vec": {"window": [5, 10, 20], "vec_size": [10, 20, 30], "epochs": [15],
                 "cls_type": ["lr"], "num_bins": [20]},
     "bert": {"input_len": [64, 128, 256], "batch_size": [128], "epochs": [20],
              "num_bins": [20]}
